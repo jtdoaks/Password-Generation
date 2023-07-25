181a4665a2acf80546ea8b546470192d2b1d6a6c
jtdoaks <136029371+jtdoaks@users.noreply.github.com>
2023-07-25 14:41:32 -0400

    added special characters

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var generateBtn = document.querySelector("#generate");
  let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 let numbers = [0,1,2,3,4,5,6,7,8,9];
+let specialCharacters =['!','@','#','$','%','^','&','*',]
 let builtArray = []
 
 
@@ -20,12 +21,21 @@ if(confirm("Do you want your password to conatin uppercase letters?"))
 builtArray =[...builtArray,  ...upperAlphabet];
 
 }
+if(confirm("Do you want your password to conatin numbers?"))
+{builtArray=[...builtArray, ...numbers];}
+
+if(confirm("Do you want your password to conatin special characters?"))
+{builtArray=[...builtArray, ...specialCharacters];}
+
 let password = "";
 
 let passwordLength =prompt('How long do you want your password to be?')
 console.log(passwordLength)
 
-for (let i = 0; i < passwordLength; i++){
+
+for (let i = 0; i < passwordLength; i++)
+
+{
 
   password += "x";
 }
